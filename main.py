3e281b6696267d16a64a1551d5060769cf8f543b
naimurborno <naimurborno@gmail.com>
2024-03-01 10:46:18 +0600

    training module has been added

--- main.py
+++ main.py
@@ -1,18 +1,23 @@
 from classifier import logger
 from classifier.pipeline.stage_01 import DataIngestionTrainPipeline
 from classifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
+from classifier.pipeline.model_trainer import ModelTrainingPipeline
+#from cnnClassifier.pipeline.stage_04_model_evaluation import EvaluationPipeline
 
-STAGE_NAME="Data Ingestion_stage"
+
+
+STAGE_NAME = "Data Ingestion stage"
 
 
 try:
-    logger.info(f">>>>>stage{STAGE_NAME} started<<<<<")
-    obj=DataIngestionTrainPipeline()
+    logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+    obj = DataIngestionTrainPipeline()
     obj.main()
-    logger.info(f">>>>>>>>stage {STAGE_NAME}completed <<<<<")
+    logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
 except Exception as e:
     logger.exception(e)
     raise e
+
 
 
 STAGE_NAME = "Prepare base model"
@@ -26,3 +31,32 @@ except Exception as e:
         logger.exception(e)
         raise e
 
+
+
+
+STAGE_NAME = "Training"
+try: 
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_trainer = ModelTrainingPipeline()
+   model_trainer.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+except Exception as e:
+        logger.exception(e)
+        raise e
+
+
+
+
+
+STAGE_NAME = "Evaluation stage"
+try:
+   logger.info(f"*******************")
+   logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+   model_evalution = EvaluationPipeline()
+   model_evalution.main()
+   logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+
+except Exception as e:
+        logger.exception(e)
+        raise e
